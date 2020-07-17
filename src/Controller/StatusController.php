2ac4d0d67b906205fb14d78705a394cdadc30c8e
Cameron Green <i@camerongreen.org>
2020-07-17 22:20:03 +1000

    Adding twig

--- src/Controller/StatusController.php
+++ src/Controller/StatusController.php
@@ -3,15 +3,21 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class StatusController {
+class StatusController extends AbstractController {
 
   /**
    * @Route("/")
    */
   public function status() {
-    return new Response('Current status');
+    return $this->render('status/status.html.twig', [
+        'statuses' => [
+          'This is the status',
+          'This is the status 2'
+        ]
+    ]);
   }
 }
